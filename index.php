4d0f45557a42fb3c8e3493cfc0e41ec8008e6f55
Carlos Sosa <carlitin@gmail.com>
2013-02-03 20:55:53 +0100

    - Rewrite __construct method - Replace save() method by saveAs() - Rename printThumbnail by printImage and add $options parameter - Rename  getThumbnailAsString by getAsString and add $options parameter - Improve autodetect of image type, now isn't is need specify image type in any method. - Remove magic methods printThumbnailAs... and saveAs... - Permit access to thumb and image resources using function getImageResource and getThumbnailResource - Minor changes in doThumbnail method - Limited support to png, gif, jpeg images.
    TODO: Document and test new features. 
    TODO: Add support for Bmp images.
    TODO: Permit add watermark to thumbnail.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-/*require 'Thumbnails.php';
+require 'Thumbnails.php';
 header("Pragma: public");
 header('Content-disposition: filename=image_thumb.png');
 header("Content-type: image/png");
 header('Content-Transfer-Encoding: binary');
 ob_clean();
 flush();
-Thumbnails::createThumb('img.jpg', null, 400,410, Thumbnails::IMAGE_CENTER | Thumbnails::IMAGE_TOUCH_OUTSIDE | Thumbnails::IMAGE_POS_RIGHT, Thumbnails::IMAGE_FORMAT_PNG, array('r'=>255,'g'=>255,'b'=>255));
-*/
-$m = base_convert('f', 16, 10);
-var_dump($m);
+$n = Thumbnails::createThumb('img.jpg', null, 1024,350, Thumbnails::IMAGE_CENTER | Thumbnails::IMAGE_TOUCH_OUTSIDE);
+$n->printThumbnail(Thumbnails::IMAGE_FORMAT_PNG);
 ?>
