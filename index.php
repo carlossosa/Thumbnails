7849fb89de224203c32cda4f4f2ca8be0be41c6b
Carlos Sosa <carlitin@gmail.com>
2013-02-03 14:14:41 +0100

    Add crop option.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'Thumbnails.php';
+/*require 'Thumbnails.php';
 header("Pragma: public");
 header('Content-disposition: filename=image_thumb.png');
 header("Content-type: image/png");
@@ -7,4 +7,7 @@ header('Content-Transfer-Encoding: binary');
 ob_clean();
 flush();
 Thumbnails::createThumb('img.jpg', null, 400,410, Thumbnails::IMAGE_CENTER | Thumbnails::IMAGE_TOUCH_OUTSIDE | Thumbnails::IMAGE_POS_RIGHT, Thumbnails::IMAGE_FORMAT_PNG, array('r'=>255,'g'=>255,'b'=>255));
+*/
+$m = base_convert('f', 16, 10);
+var_dump($m);
 ?>
